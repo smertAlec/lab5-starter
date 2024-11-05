2d60b133ea4f27e3ae62a136ead360f85c24670b
Alec Fong <a9fong@ieng6-201.ucsd.edu>
2024-11-04 20:01:15 -0800

    some changes

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: 
+	gcc -std=c11 -Wall -Wno-unused-variable -fsanitize=address -g number-server.c http-server.c -o number-server
--- number-server.c
+++ number-server.c
@@ -11,6 +11,8 @@ void handle_404(int client_sock, char *path)  {
     // snprintf includes a null-terminator
 
     // TODO: send response back to client?
+    write(client_sock, HTTP_404_NOT_FOUND, strlen(HTTP_404_NOT_FOUND));
+    write(client_sock, "Error message\r\n", strlen("Error message\r\n"));
 }
 
 
